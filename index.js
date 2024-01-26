11c906bc433b45e20f1d0bed3729472515049fb0
Samantha <samanthavillafana8@gmail.com>
2024-01-25 20:56:32 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+/*
+I need to make hasTargetSum function that checks if two numbers in an array add up to some target number
+For example if the array is [1,2,3,4] and the target number is 6, I know that 2 and 4 add up to 6 so it should
+return true. If I have the same array and the target is 10, no two numbers in the array add up to 10 so it should
+return false
+*/
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    const complement = target - array[i];
+    for (let j = i + 1; j <array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
@@ -8,6 +22,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  Iterate over the array of numbers
+  for the current number, identify a cmplementary number that adds to our target
+  i.e. if our number is 2 and the target is 5, the complementary number is 3
+  iterate over the remaining numbers in the arrray
+  check if any of the remaining numbers is the complement
+  if so, return true
+  if we reach the end of the array, return false
 */
 
 /*
@@ -29,6 +50,22 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+    // Negative numbers?
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+  
+    console.log("");
+    // Multiple pairs?
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+  
+    console.log("");
+    // Single numbers?
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([4], 4));
+
 }
 
 module.exports = hasTargetSum;
